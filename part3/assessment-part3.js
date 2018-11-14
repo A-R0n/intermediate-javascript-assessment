6159bacbe8a924c2c5ce1dd3bffe15e6a5a4d856
Aaron Estes <aestesc@gmail.com>
2018-11-14 16:53:51 -0600

    third assessment incomplete

--- part3/assessment-part3.js
+++ part3/assessment-part3.js
@@ -13,7 +13,9 @@
 // return the result of your updateAnimal invocation
 
 // CODE HERE...
-
+function callBinding(magicAnimals, updateAnimal, id){
+    
+}
 
 
 // *************
@@ -28,7 +30,9 @@
 // return the result of your updateAnimal invocation
 
 // CODE HERE...
+function applyBinding(magicAnimals, updateAnimal, id){
 
+}
 
 
 // *************
@@ -48,7 +52,9 @@
 var foo;
 
 // CODE HERE...
+function promiseMe($q){
 
+}
 
 
 // *************
@@ -64,3 +70,6 @@ var foo;
 // and then resolve the array as you complete your promise.
 
 // CODE HERE...
+function emailList($q, $http){
+    
+}
